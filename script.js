0a023002d2c96d4a3ce528c70b4be3f81537316c
Kuldeep Singh <986893ks@gmail.com>
2024-09-22 00:03:26 +0530

    Page1 bug handled

--- script.js
+++ script.js
@@ -41,11 +41,11 @@ tl.from("#page1",{
 tl.to("#loader",{
   display:"none"
 })
-tl.from("#nac",{
+tl.from("#nav",{
   opacity:0
 })
 tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero3 h3, #hero4 h1",{
-  y:120,
+  y:150,
   stagger:0.2
 })
 }
